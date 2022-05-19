865ace9d5c260988201004d3a1a0c9411a7ddaa1
Kyler <kylerlepp8@gmail.com>
2022-05-19 14:28:26 -0700

    refactored post method and fixed cross scripting along with i added error messages for if your over 140 chars and if the field is empty

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,57 +3,65 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function() {
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1652639242172
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1652725642172
-    }
-  ]
-  
-  const createTweetElement = function(tweetData) {
-    const $tweet = (`<section class="sec">
+
+
+
+  const createTweetElement = function (tweetData) {
+    const $tweet = $(`<article class="sec">
     <header class="users-name">
       <img src="${tweetData.user.avatars}">
-     ${tweetData.user.name}
     </header>
+    <p> ${tweetData.user.name} </p>
     <header class="blue">${tweetData.user.handle}</header>
-    <article>
-      ${tweetData.content.text}
+    <article class="line">
+      ${$('<span>').text(tweetData.content.text).html()}
     </article>
     <footer>
-      ${tweetData.created_at}
+      ${timeago.format(tweetData.created_at)}
     </footer>
     <span class="icons"><i class="fa-solid fa-flag"></i></span>
       <span class="icons2"><i class="fa-solid fa-retweet"></i></span>
       <span class="icons3"><i class="fa-solid fa-heart"></i></span>
-    </section>`);
+    </article>`);
     return $tweet;
   };
-  
-  const renderTweets = function(tweets) {
-    for(const prop of tweets) {
-      const $tweets = createTweetElement(prop);
-      $('.container').append($tweets);
-    }
+
+  const renderTweets = function (tweets) {
+    tweets.forEach(tweet => {
+      $('.tweet-container').prepend(createTweetElement(tweet));
+    })
   };
-  renderTweets(tweetData);
+  const loadTweets = function () {
+    $.get('/tweets')
+      .then(response => {
+        renderTweets(response);
+      });
+  };
+  $(document).ready(() => {
+  loadTweets();
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+    if (!$('#tweet-text').val()) {
+      $('.no-text').slideDown('slow', function(){
+      })
+      return;
+    }
+    if ($('#tweet-text').val().length > 140) {
+      $('.too-many-chars').slideDown('slow', function() {
+      });
+      return;
+    }
+      const $tweetBody = $(this).serialize();
+      $.post('/tweets', $tweetBody)
+        .then(response => {
+          $.get('/tweets', (data) => {
+            console.log(data);
+            $('textarea').val("")
+            renderTweets(data.slice(-1))
+          })
+          $('.too-many-chars').css('display', 'none');
+          $('.no-text').css('display', 'none');
+          $('#counterr').text(140);
+        })
+  })
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  $("#tweet-text").on('keypress', function() {
-    const max = 139;
+  $("#tweet-text").on('input', function() {
+    const max = 140;
     const remaining = max - $(this).val().length;
     $("#counterr").text(remaining);
     const color = remaining < 0 ? $("#counterr").css('color', 'red') : null;
-    
+    remaining > 0 ? $('#counterr').css('color', '#424242') : null;
   });
 })
