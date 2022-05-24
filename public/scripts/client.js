43a24286063e2026034e8bab474aa486705d9f02
Kyler <kylerlepp8@gmail.com>
2022-05-24 13:42:18 -0700

    added photos for the readme updated readme a little and cleaned up all files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,9 +5,9 @@
  */
 
 
-
-  const createTweetElement = function (tweetData) {
-    const $tweet = $(`<article class="sec">
+// function to create the tweet element based on the layout provided
+const createTweetElement = function(tweetData) {
+  const $tweet = $(`<article class="sec">
     <header class="users-name">
       <img src="${tweetData.user.avatars}">
     </header>
@@ -23,27 +23,29 @@
       <span class="icons2"><i class="fa-solid fa-retweet"></i></span>
       <span class="icons3"><i class="fa-solid fa-heart"></i></span>
     </article>`);
-    return $tweet;
-  };
-
-  const renderTweets = function (tweets) {
-    tweets.forEach(tweet => {
-      $('.tweet-container').prepend(createTweetElement(tweet));
-    })
-  };
-  const loadTweets = function () {
-    $.get('/tweets')
-      .then(response => {
-        renderTweets(response);
-      });
-  };
-  $(document).ready(() => {
+  return $tweet;
+};
+//function to load the tweets into the html container
+const renderTweets = function(tweets) {
+  tweets.forEach(tweet => {
+    $('.tweet-container').prepend(createTweetElement(tweet));
+  });
+};
+//this will retrieve the tweet data from the database
+const loadTweets = function() {
+  $.get('/tweets')
+    .then(response => {
+      renderTweets(response);
+    });
+};
+// below is the code to slide down the error messages if an error is present
+$(document).ready(() => {
   loadTweets();
-  $('form').on('submit', function (event) {
+  $('form').on('submit', function(event) {
     event.preventDefault();
     if (!$('#tweet-text').val()) {
-      $('.no-text').slideDown('slow', function(){
-      })
+      $('.no-text').slideDown('slow', function() {
+      });
       return;
     }
     if ($('#tweet-text').val().length > 140) {
@@ -51,17 +53,18 @@
       });
       return;
     }
-      const $tweetBody = $(this).serialize();
-      $.post('/tweets', $tweetBody)
-        .then(response => {
-          $.get('/tweets', (data) => {
-            console.log(data);
-            $('textarea').val("")
-            renderTweets(data.slice(-1))
-          })
-          $('.too-many-chars').css('display', 'none');
-          $('.no-text').css('display', 'none');
-          $('#counterr').text(140);
-        })
-  })
+    //this is the code to serialize the text from the user and then when a tweet is posted it makes sure the counter is set to 140 and the display of the error messages is off if no error is present.
+    const $tweetBody = $(this).serialize();
+    $.post('/tweets', $tweetBody)
+      .then(response => {
+        $.get('/tweets', (data) => {
+          console.log(data);
+          $('textarea').val("");
+          renderTweets(data.slice(-1));
+        });
+        $('.too-many-chars').css('display', 'none');
+        $('.no-text').css('display', 'none');
+        $('#counterr').text(140);
+      });
+  });
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,11 @@
+//function to change counter number based on keyboard input and changes color if too many chars
+
 $(document).ready(function() {
   $("#tweet-text").on('input', function() {
     const max = 140;
     const remaining = max - $(this).val().length;
     $("#counterr").text(remaining);
-    const color = remaining < 0 ? $("#counterr").css('color', 'red') : null;
+    remaining < 0 ? $("#counterr").css('color', 'red') : null;
     remaining > 0 ? $('#counterr').css('color', '#424242') : null;
   });
-})
+});
